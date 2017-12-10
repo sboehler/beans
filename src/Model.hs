3d666c1f4b058d0f0227778a01a9fae80ba7c4be
sboehler <sboehler@noreply.users.github.com>
2017-12-10 23:52:45 +0100

    Some primitives

---
+++ src/Model.hs
@@ -0,0 +1,31 @@
+module Model where
+
+import Data.Decimal (Decimal)
+import Data.Text (Text)
+import Data.Time.Calendar (Day)
+
+data Transaction = T
+  { date :: Day
+  , description :: Text
+  , postings :: [Posting]
+  } deriving (Show)
+
+data Posting
+  = Posting { accountName :: AccountName
+            , amount :: Decimal
+            , commodity :: Commodity }
+  | WildcardPosting { account :: Account }
+  deriving (Show)
+
+newtype Commodity =
+  Commodity Text
+  deriving (Show)
+
+newtype AccountName =
+  AccountName [Text]
+  deriving (Show)
+
+data Account = Account
+  { accountName :: AccountName
+  , commodities :: [Commodity]
+  } deriving (Show)

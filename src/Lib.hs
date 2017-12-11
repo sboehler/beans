2418d540d785da3de338f7be62c62b702154de90
sboehler <sboehler@noreply.users.github.com>
2017-12-11 22:29:59 +0100

    Parse file with transactions

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,15 @@
 module Lib
-    ( someFunc
-    ) where
+  ( doParse
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Text (pack)
+import Parser (parse')
+import System.Environment (getArgs)
+
+doParse :: IO ()
+doParse = do
+  (file:_) <- getArgs
+  s <- readFile file
+  case parse' file (pack s) of
+    Left x -> print x
+    Right t -> print t

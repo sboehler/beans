020bace74cea69f726c7b0f00f60434b86885c9a
sboehler <sboehler@noreply.users.github.com>
2018-01-03 00:01:42 +0100

    New operator to convert an amount to another commodity

--- src/Data/Price.hs
+++ src/Data/Price.hs
@@ -1,8 +1,9 @@
 module Data.Price
   ( Price(..)
+  , (<@>)
   ) where
 
-import Data.Amount (Amount)
+import Data.Amount (Amount(..))
 import Data.Commodity (CommodityName)
 import Data.Text.Prettyprint.Doc (Pretty, pretty)
 
@@ -11,5 +12,13 @@ data Price a = Price
   , _amount :: Amount a
   } deriving (Eq, Show, Functor)
 
-instance Show a => Pretty (Price a) where
+instance Show a =>
+         Pretty (Price a) where
   pretty Price {..} = pretty _amount
+
+(<@>)
+  :: Num a
+  => Amount a -> Price a -> Amount a
+(Amount a c) <@> (Price psc pa)
+  | psc == c = (* a) <$> pa
+  | otherwise = error "Source commodity must match with amount commodity"

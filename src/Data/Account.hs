27db3bd87e4a0f01be30c37f66a8e6740c5f4a5a
sboehler <sboehler@noreply.users.github.com>
2018-01-28 14:46:17 +0100

    Simplify Account and Accounts

--- src/Data/Account.hs
+++ src/Data/Account.hs
@@ -1,28 +1,17 @@
 module Data.Account
   ( Account(..)
-  , Accounts(..)
-  , insert
   , addPosting
   , AccountName(..)
   ) where
 
 import Data.AccountName (AccountName(..))
+import qualified Data.Accounts as A
 import Data.Commodity (CommodityName)
 import qualified Data.Holdings as H
-import qualified Data.Map.Lazy as M
 import Data.Posting (Posting(..))
-import Data.Text.Lazy (Text)
-
-newtype Accounts a = Accounts
-  { _unAccounts :: M.Map Text (Account a)
-  } deriving (Show, Eq)
-
-instance Num a => Monoid (Accounts a) where
-  mempty = Accounts mempty
-  mappend (Accounts a) (Accounts a') = Accounts $ M.unionWith mappend a a'
 
 data Account a = Account
-  { _accounts :: Accounts a
+  { _accounts :: A.Accounts (Account a)
   , _holdings :: H.Holdings a
   } deriving (Show, Eq)
 
@@ -31,14 +20,13 @@ instance Num a => Monoid (Account a) where
   (Account a h) `mappend` (Account a' h') =
     Account (a `mappend` a') (h `mappend` h')
 
-insert :: Num a => Account a -> CommodityName -> a -> Account a
-insert (Account accounts h) c a = Account accounts (H.insert h c a)
-
-addPosting :: Num a => Account a -> Posting a -> Account a
-addPosting account Posting {..} = f (_unAccountName _accountName) account
-  where
-    f (n:ns) (Account (Accounts accounts) h) =
-      Account (Accounts $ M.insert n account' accounts) h
-      where
-        account' = f ns (M.findWithDefault mempty n accounts)
-    f [] (Account a h) = Account a (H.insert h _commodity _amount)
+addPosting :: Num a => Posting a -> Account a -> Account a
+addPosting p@Posting {..} (Account a h) =
+  case _accountName of
+    (AccountName (n:ns)) ->
+      let p' = p {_accountName = AccountName ns}
+          a' = A.adjust (addPosting p') n a
+      in Account a' h
+    (AccountName []) ->
+      let h' = H.insert h _commodity _amount
+      in Account a h'

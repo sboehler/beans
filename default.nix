6917914605cb036f9869c5902aa1458da69278ff
Silvio Böhler <sboehler@noreply.users.github.com>
2018-03-23 00:57:39 +0100

    Add hindent

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 { mkDerivation, base, containers, Decimal, exceptions, filepath
 , free, lens, mtl, parsec, parsec3-numbers, prettyprinter, stdenv
-, text, time, apply-refact, hlint, hasktags, hoogle, stylish-haskell
+, text, time, apply-refact, hlint, hasktags, hoogle, stylish-haskell, hindent
 }:
 mkDerivation {
   pname = "haricot";
   version = "0.1.0.0";
   src = ./.;
   isExecutable = true;
-  buildDepends = [apply-refact hlint hasktags hoogle stylish-haskell];
+  buildDepends = [apply-refact hlint hasktags hoogle stylish-haskell hindent];
   executableHaskellDepends = [
     base containers Decimal exceptions filepath free lens mtl parsec
     parsec3-numbers prettyprinter text time

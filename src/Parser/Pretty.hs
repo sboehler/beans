6cf372628c9c468f41b7b814e5649b2fd8b397ee
Silvio Böhler <sboehler@noreply.users.github.com>
2018-03-31 00:14:29 +0200

    Refactoring, simplify data directory

--- src/Parser/Pretty.hs
+++ src/Parser/Pretty.hs
@@ -2,38 +2,17 @@
 
 module Parser.Pretty where
 
-import qualified Data.Map.Lazy as M
-import Data.Text.Prettyprint.Doc
-       (Pretty, (<+>), (<>), align, cat, dquotes, encloseSep, indent,
-        line, nest, pretty, sep, vcat, vsep)
+import Data.Text.Prettyprint.Doc ( Pretty , (<+>) , (<>) , cat , dquotes , encloseSep , indent , line , pretty , sep , vcat)
+import Data.Scientific (Scientific)
 import Data.Time.Calendar (Day)
 
-import Data.Account (Account(..))
-import Data.AccountName (AccountName(..))
-import Data.Accounts (Accounts(..))
-import Data.Amount (Amount(..))
-import Data.Commodity (CommodityName(..))
-import Data.Holdings (Holdings(..))
-import Data.Lot (Lot(..))
-import Data.Posting (Posting(..), PostingPrice(..))
-import Data.Transaction (Flag(..), Tag(..), Transaction(..))
 import Parser.AST
-       (Balance(..), Close(..), Directive(..), Include(..), Open(..),
-        Option(..), PostingDirective(..), Price(..))
 
-instance Pretty Account where
-  pretty (Account a h) = pretty h <+> line <+> nest 2 (pretty a)
-
-instance (Pretty a) => Pretty (Accounts a) where
-  pretty (Accounts m) = vsep (fmap f (M.toList m))
-    where
-      f (name, account) = pretty name <+> pretty account
+instance Pretty Scientific where
+  pretty = pretty . show
 
 instance Pretty AccountName where
   pretty = pretty . show
-
-instance Pretty Amount where
-  pretty Amount {..} = pretty (show _amount) <+> pretty _commodity
 
 instance Pretty CommodityName where
   pretty = pretty . _unCommodityName
@@ -55,28 +34,19 @@ instance Pretty Flag where
 instance Pretty Tag where
   pretty (Tag t) = pretty t
 
-instance Pretty Holdings where
-  pretty (Holdings h) = align $ vsep (map f (M.toList h))
-    where
-      f (c, a) = (pretty . show) a <+> pretty c
-
 instance Pretty Lot where
-  pretty Lot {..} =
+  pretty Lot { _label, _price, _targetCommodity, _date } =
     encloseSep "{" "}" "," $
-    [pretty _cost, pretty _date] ++
+    [pretty _price, pretty _targetCommodity, pretty _date] ++
     case _label of
       Nothing -> []
       _ -> [pretty _label]
 
 instance Pretty Posting where
-  pretty Posting {..} =
-    pretty _accountName <+>
-    (pretty . show) _amount <+>
-    pretty _commodity <+> pretty _price <+> pretty _lot
-
-instance Pretty PostingPrice where
-  pretty (UnitPrice p) = "@" <+> pretty p
-  pretty (TotalPrice a) = "@@" <+> pretty a
+  pretty Posting {_account, _amount, _commodity, _lot} =
+    pretty _account <+>
+    (pretty . show) _amount <+> pretty _commodity <+> pretty _lot
+  pretty (Wildcard a) = pretty a
 
 instance Pretty (Directive a) where
   pretty (Opn x _) = pretty x
@@ -88,8 +58,8 @@ instance Pretty (Directive a) where
   pretty (Inc x _) = pretty x
 
 instance Pretty Balance where
-  pretty Balance {..} =
-    pretty _date <+> "balance" <+> pretty _accountName <+> pretty _amount
+  pretty Balance { _date, _account, _amount, _commodity} =
+    pretty _date <+> "balance" <+> pretty _account <+> pretty _amount <+> pretty _commodity
 
 instance Pretty Open where
   pretty Open {..} =
@@ -97,17 +67,13 @@ instance Pretty Open where
     "open" <+> pretty _accountName <+> sep (map pretty _commodities)
 
 instance Pretty Close where
-  pretty Close {..} = pretty _date <+> "close" <+> pretty _accountName
+  pretty Close {..} = pretty _date <+> "close" <+> pretty _account
 
 instance Pretty Price where
-  pretty Price {..} = pretty _date <+> "price" <+> pretty _price
+  pretty Price {..} = pretty _date <+> "price" <+> pretty _commodity <+> pretty _price <+> pretty _targetCommodity
 
 instance Pretty Include where
   pretty (Include filePath) = "include" <+> pretty filePath
 
 instance Pretty Option where
   pretty (Option d t) = "option" <+> pretty d <+> pretty t
-
-instance Pretty PostingDirective where
-  pretty (CompletePosting p) = pretty p
-  pretty (WildcardPosting n) = pretty n

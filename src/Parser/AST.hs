3ba77fbfdaf670d91338e70767ae079681d6a8c7
Silvio Böhler <sboehler@noreply.users.github.com>
2018-03-31 18:47:05 +0200

    Refactor parser & data model
    * new data type "Event" to encapsulate dated directives
    * common date parsing for all events
    * new indentation handling

--- src/Parser/AST.hs
+++ src/Parser/AST.hs
@@ -5,50 +5,49 @@ import           Data.Text.Lazy     (Text, intercalate, unpack)
 import           Data.Time.Calendar (Day)
 
 data Directive a
-  = Opn Open
-        a
-  | Bal Balance
-        a
-  | Trn Transaction
-        a
-  | Cls Close
-        a
-  | Prc Price
-        a
+  = Evt Event a
   | Opt Option
         a
   | Inc Include
         a
   deriving (Eq, Show, Functor, Ord)
 
+data Event
+  = Bal Day
+        Balance
+  | Opn Day
+        Open
+  | Cls Day
+        Close
+  | Trn Day
+        Transaction
+  | Prc Day
+        Price
+  deriving (Eq, Show, Ord)
+
 data Balance = Balance
-  { _date      :: Day
-  , _account   :: AccountName
+  { _account   :: AccountName
   , _amount    :: Scientific
   , _commodity :: CommodityName
   } deriving (Eq, Show, Ord)
 
 data Open = Open
-  { _date        :: Day
-  , _accountName :: AccountName
+  { _account     :: AccountName
   , _commodities :: [CommodityName]
   } deriving (Show, Eq, Ord)
 
-data Close = Close
-  { _date    :: Day
-  , _account :: AccountName
+newtype Close = Close
+  { _account :: AccountName
   } deriving (Show, Eq, Ord)
 
 data Price = Price
-  { _date            :: Day
-  , _commodity       :: CommodityName
+  { _commodity       :: CommodityName
   , _price           :: Scientific
   , _targetCommodity :: CommodityName
   } deriving (Show, Eq, Ord)
 
 data Transaction = Transaction
-  { _date        :: Day
-  , _flag        :: Flag
+  { _flag        :: Flag
   , _description :: Text
   , _tags        :: [Tag]
   , _postings    :: [Posting]

33575d133334abd5c2465f13250001c3ec9be363
Silvio Böhler <sboehler@noreply.users.github.com>
2018-04-08 17:16:03 +0200

    Move completing postings back into parser
    Remove again generics from transactions and directives, turned out to be less
    useful

--- src/Haricot/Parser.hs
+++ src/Haricot/Parser.hs
@@ -1,16 +1,17 @@
 module Haricot.Parser where
 
 import           Control.Monad              (void)
-import           Control.Monad.Catch        (MonadThrow, throwM)
+import           Control.Monad.Catch        (Exception, MonadThrow, throwM)
 import           Control.Monad.IO.Class     (MonadIO)
 import           Control.Monad.Trans        (liftIO)
 import           Data.Char                  (isAlphaNum)
 import           Data.Functor               (($>))
+import qualified Data.Map.Strict            as M
 import           Data.Scientific            (Scientific)
+import qualified Data.Set                   as S
 import           Data.Text.Lazy             (Text, cons, unpack)
 import           Data.Text.Lazy.IO          (readFile)
 import           Data.Time.Calendar         (Day, fromGregorian)
-import           Data.Void                  (Void)
 import           Haricot.AST
 import           Prelude                    hiding (readFile)
 import           System.FilePath.Posix      (combine, takeDirectory)
@@ -19,7 +20,20 @@ import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 import qualified Text.Megaparsec.Pos        as P
 
-type Parser = Parsec Void Text
+newtype VerifyException =
+  UnbalancedTransaction P.SourcePos
+  deriving (Eq, Show, Ord)
+
+instance Exception VerifyException
+
+instance ShowErrorComponent VerifyException where
+  showErrorComponent (UnbalancedTransaction pos) =
+    "Unbalanced transaction: " ++ show pos
+
+verifyError :: P.SourcePos -> Parser a
+verifyError = fancyFailure . S.singleton . ErrorCustom . UnbalancedTransaction
+
+type Parser = Parsec VerifyException Text
 
 lineComment :: Parser ()
 lineComment =
@@ -79,19 +93,28 @@ lot d = braces $ Lot <$> number <*> commodity <*> lotDate <*> lotLabel
 postingPrice :: Parser ()
 postingPrice = (symbol "@" *> optional (symbol "@") *> number *> commodity) $> ()
 
-completePosting :: Day -> P.SourcePos -> AccountName -> Parser CompletePosting
-completePosting d p a =
-  CompletePosting p a <$> number <*> commodity <*> optional (lot d) <*
+data PostingDirective
+  = CP Posting
+  | WP WildcardPosting
+
+data WildcardPosting = WildcardPosting
+  { _pos     :: P.SourcePos
+  , _account :: AccountName
+  } deriving (Show, Eq)
+
+posting :: Day -> P.SourcePos -> AccountName -> Parser Posting
+posting d p a =
+  Posting p a <$> number <*> commodity <*> optional (lot d) <*
   optional postingPrice
 
 wildcardPosting :: P.SourcePos -> AccountName -> Parser WildcardPosting
 wildcardPosting p a = return $ WildcardPosting p a
 
-posting :: Day -> Parser Posting
-posting d = do
+postingDirective :: Day -> Parser PostingDirective
+postingDirective d = do
   pos <- getPosition
   a <- account
-  CP <$> completePosting d pos a <|> WP <$> wildcardPosting pos a
+  CP <$> posting d pos a <|> WP <$> wildcardPosting pos a
 
 flag :: Parser Flag
 flag = complete <|> incomplete
@@ -102,14 +125,18 @@ flag = complete <|> incomplete
 tag :: Parser Tag
 tag = Tag <$> (cons <$> char '#' <*> takeWhile1P (Just "alphanum") isAlphaNum)
 
-transaction :: P.SourcePos -> Day -> Parser (Transaction [Posting])
+transaction :: P.SourcePos -> Day -> Parser Transaction
 transaction pos d = do
   f <- flag
   desc <- quotedString
   t <- many tag
   indent <- L.indentGuard scn GT P.pos1
-  p <- some $ try (L.indentGuard scn EQ indent *> posting d)
-  return $ Transaction pos d f desc t p
+  p <- some $ try (L.indentGuard scn EQ indent *> postingDirective d)
+  let postings = completePostings pos p
+  case postings of
+    Left _ -> verifyError pos
+    Right p' -> return $ Transaction pos d f desc t p'
+
 
 open :: P.SourcePos -> Day -> Parser Open
 open pos d = Open pos d <$ symbol "open" <*> account <*> restriction
@@ -128,7 +155,7 @@ price :: P.SourcePos -> Day -> Parser Price
 price pos d =
   Price pos d <$ symbol "price" <*> commodity <*> number <*> commodity
 
-event :: Parser (Directive [Posting])
+event :: Parser Directive
 event = do
   pos <- getPosition
   d <- date
@@ -142,27 +169,54 @@ include = symbol "include" >> Include <$> getPosition <*> (unpack <$> quotedStri
 config :: Parser Option
 config = symbol "option" >> Option <$> getPosition <*> quotedString <*> quotedString
 
-directive :: Parser (Directive [Posting])
+directive :: Parser Directive
 directive =
   L.nonIndented scn $
   (event <|> Inc <$> include <|> Opt <$> config) <* scn
 
-directives :: Parser [Directive [Posting]]
+directives :: Parser [Directive ]
 directives = some directive <* eof
 
-parseSource :: (MonadThrow m) => FilePath -> Text -> m [Directive [Posting]]
+parseSource :: (MonadThrow m) => FilePath -> Text -> m [Directive ]
 parseSource f t =
   case parse directives f t of
     Left e  -> throwM e
     Right d -> return d
 
-getIncludedFiles :: FilePath -> [Directive [Posting]] -> [FilePath]
+getIncludedFiles :: FilePath -> [Directive] -> [FilePath]
 getIncludedFiles fp ast =
   [combine (takeDirectory fp) path | (Inc (Include _ path)) <- ast]
 
-parseFile :: (MonadIO m, MonadThrow m) => FilePath -> m [Directive [Posting]]
+parseFile :: (MonadIO m, MonadThrow m) => FilePath -> m [Directive]
 parseFile filePath = do
   source <- liftIO $ readFile filePath
   ast <- parseSource filePath source
   asts <- concat <$> traverse parseFile (getIncludedFiles filePath ast)
   return $ ast ++ asts
+
+completePostings :: (MonadThrow m) => P.SourcePos -> [PostingDirective] -> m [Posting]
+completePostings pos p =
+  let wildcards = [w | WP w <- p]
+      postings = [c | CP c <- p]
+   in case calculateImbalances postings of
+        [] -> return postings
+        imbalances ->
+          case wildcards of
+            [w] -> return $ postings ++ map (balanceImbalance w) imbalances
+            _   -> throwM $ UnbalancedTransaction pos
+
+balanceImbalance :: WildcardPosting -> (CommodityName, Scientific) -> Posting
+balanceImbalance WildcardPosting {_pos, _account} (c, a) =
+  Posting
+    {_amount = negate a, _commodity = c, _lot = Nothing, ..}
+
+calculateImbalances :: [Posting] -> [(CommodityName, Scientific)]
+calculateImbalances =
+  M.toList . M.filter ((> 0.005) . abs) . M.fromListWith (+) . fmap weight
+
+weight :: Posting  -> (CommodityName, Scientific)
+weight Posting {..} =
+  case _lot of
+    Just Lot {_price, _targetCommodity} ->
+      (_targetCommodity, _amount * _price)
+    _ -> (_commodity, _amount)

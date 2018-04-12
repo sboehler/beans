41b4322847f181ca554c9e7b49110d1f2934fdcb
Silvio Böhler <sboehler@noreply.users.github.com>
2018-04-13 01:12:44 +0200

    Fix wrong traversal direction

--- src/Haricot/Prices.hs
+++ src/Haricot/Prices.hs
@@ -6,7 +6,7 @@ module Haricot.Prices
   ) where
 
 import qualified Data.Map.Strict    as M
-import           Data.Scientific
+import           Data.Scientific    (Scientific)
 import           Data.Time.Calendar (Day)
 import           Haricot.AST
 import           Haricot.Ledger
@@ -16,9 +16,9 @@ type PricesHistory = M.Map Day Prices
 type Prices = M.Map CommodityName (M.Map CommodityName Scientific)
 
 calculatePrices :: Ledger -> PricesHistory
-calculatePrices l = fst $ foldr f (M.empty, M.empty) l
+calculatePrices l = fst $ foldl f (M.empty, M.empty) l
   where
-    f ts@Timestep {_date} (accountsHistory, latest) =
+    f  (accountsHistory, latest) ts@Timestep {_date}=
       let latest' = updatePrices ts latest
        in (M.insert _date latest' accountsHistory, latest')
 

02f0d79a6e5b7a6bc06b2071fbe67a0484bb65d7
Silvio Böhler <sboehler@noreply.users.github.com>
2018-04-12 23:38:17 +0200

    Simplifications

--- src/Haricot/Accounts.hs
+++ src/Haricot/Accounts.hs
@@ -7,9 +7,8 @@ module Haricot.Accounts
   , calculateAccounts
   ) where
 
-import           Control.Monad       (when)
 import           Control.Monad.Catch
-import           Data.Foldable       (foldlM, foldrM)
+import           Data.Foldable       (foldrM)
 import qualified Data.Map.Strict     as M
 import           Data.Scientific
 import           Data.Time.Calendar  (Day)
@@ -20,10 +19,10 @@ type AccountsHistory = M.Map Day Accounts
 
 type Accounts = M.Map AccountName Account
 
-data Account = Account {
-  _restriction :: Restriction,
-  _holdings    :: Holdings
-} deriving (Show)
+data Account = Account
+  { _restriction :: Restriction
+  , _holdings    :: Holdings
+  } deriving (Show)
 
 type Holdings = M.Map CommodityName Lots
 
@@ -31,7 +30,8 @@ type Lots = M.Map Lot Scientific
 
 instance Monoid Account where
   mempty = Account NoRestriction M.empty
-  Account _ l1 `mappend` Account _ l2 = Account NoRestriction (M.unionWith (M.unionWith (+)) l1 l2)
+  Account _ l1 `mappend` Account _ l2 =
+    Account NoRestriction (M.unionWith (M.unionWith (+)) l1 l2)
 
 data AccountsException
   = AccountIsNotOpen Close
@@ -39,24 +39,28 @@ data AccountsException
   | AccountIsAlreadyOpen Open
   | BalanceIsNotZero Close
   | AccountDoesNotExist Balance
-  | BalanceDoesNotMatch Balance Scientific
+  | BalanceDoesNotMatch Balance
+                        Scientific
   deriving (Show)
 
 instance Exception AccountsException
 
 calculateAccounts :: (MonadThrow m) => Ledger -> m AccountsHistory
-calculateAccounts l = fst <$> foldlM f (M.empty, M.empty) l
+calculateAccounts l = fst <$> foldrM f (M.empty, M.empty) l
   where
-    f (accountsHistory, latest) ts@Timestep {_date} = do
+    f ts@Timestep {_date} (accountsHistory, latest) = do
       latest' <- updateAccounts ts latest
       return (M.insert _date latest' accountsHistory, latest')
 
 updateAccounts :: (MonadThrow m) => Timestep -> Accounts -> m Accounts
-updateAccounts Timestep {..} accounts = do
-  accounts' <- foldrM openAccount accounts _openings
-  accounts'' <- foldrM closeAccount accounts' _closings
-  _ <- mapM_ (checkBalance accounts'') _balances
-  foldrM bookTransaction accounts'' _transactions
+updateAccounts Timestep {..} accounts =
+  pure accounts >>= openAccounts >>= closeAccounts >>= checkBalances >>=
+  bookTransactions
+  where
+    openAccounts a = foldrM openAccount a _openings
+    closeAccounts a = foldrM closeAccount a _closings
+    checkBalances a = foldrM checkBalance a _balances
+    bookTransactions a = foldrM bookTransaction a _transactions
 
 openAccount :: MonadThrow m => Open -> Accounts -> m Accounts
 openAccount open@Open {_account, _restriction} accounts =
@@ -65,7 +69,7 @@ openAccount open@Open {_account, _restriction} accounts =
       return $ M.insert _account (Account _restriction M.empty) accounts
     Just _ -> throwM $ AccountIsAlreadyOpen open
 
-closeAccount :: MonadThrow m =>  Close -> Accounts -> m Accounts
+closeAccount :: MonadThrow m => Close -> Accounts -> m Accounts
 closeAccount closing@Close {..} accounts =
   case M.lookup _account accounts of
     Just Account {_holdings}
@@ -73,41 +77,41 @@ closeAccount closing@Close {..} accounts =
       | otherwise -> throwM $ BalanceIsNotZero closing
     Nothing -> throwM $ AccountIsNotOpen closing
 
-checkBalance :: MonadThrow m => Accounts -> Balance -> m ()
-checkBalance a bal@Balance {_account, _amount, _commodity} =
-  case M.lookup _account a of
+checkBalance :: MonadThrow m => Balance -> Accounts -> m Accounts
+checkBalance bal@Balance {_account, _amount, _commodity} accounts =
+  case M.lookup _account accounts of
     Nothing -> throwM $ AccountDoesNotExist bal
     Just Account {_holdings} ->
-      let amount' = calculateAmount _holdings _commodity
-       in when (amount' /= _amount) $ throwM (BalanceDoesNotMatch bal amount')
+      let amount' = calculateAmount _commodity _holdings
+       in if amount' /= _amount
+            then throwM (BalanceDoesNotMatch bal amount')
+            else return accounts
 
-calculateAmount :: Holdings -> CommodityName -> Scientific
-calculateAmount h c = case M.lookup c h of
-  Just m  -> sum m
-  Nothing -> 0
+calculateAmount :: CommodityName -> Holdings -> Scientific
+calculateAmount commodity = sum . M.findWithDefault M.empty commodity
 
-bookTransaction :: MonadThrow m => Transaction -> Accounts  -> m Accounts
-bookTransaction Transaction{_postings} a = foldlM book a _postings
+bookTransaction :: MonadThrow m => Transaction -> Accounts -> m Accounts
+bookTransaction Transaction {_postings} accounts =
+  foldrM book accounts _postings
 
-book :: MonadThrow m => Accounts -> Posting -> m Accounts
-book accounts p@Posting {_account, _commodity} =
+book :: MonadThrow m => Posting -> Accounts -> m Accounts
+book p@Posting {_account, _commodity} accounts =
   case M.lookup _account accounts of
     Just a -> do
-      a' <- updateAccount a p
+      a' <- updateAccount p a
       return $ M.insert _account a' accounts
     _ -> throwM $ BookingErrorAccountNotOpen p
 
-updateAccount :: MonadThrow m => Account -> Posting -> m Account
-updateAccount a@Account {_restriction, _holdings} p@Posting {_commodity} =
+updateAccount :: MonadThrow m => Posting -> Account -> m Account
+updateAccount p@Posting {_commodity} a@Account {_restriction, _holdings} =
   if _commodity `compatibleWith` _restriction
-    then return $ a {_holdings = updateHoldings _holdings p}
+    then return $ a {_holdings = updateHoldings p _holdings}
     else throwM $ BookingErrorAccountNotOpen p
 
-updateHoldings :: Holdings -> Posting -> Holdings
-updateHoldings h p@Posting {_lot, _amount, _commodity} =
+updateHoldings :: Posting -> Holdings -> Holdings
+updateHoldings p@Posting {_lot, _amount, _commodity} h =
   let lots = M.findWithDefault M.empty _commodity h
-   in M.insert _commodity (updateLot lots p) h
+   in M.insert _commodity (updateLot p lots) h
 
-updateLot :: Lots -> Posting -> Lots
-updateLot lots Posting {_lot, _amount} =
-  M.insertWith (+) _lot _amount lots
+updateLot :: Posting -> Lots -> Lots
+updateLot Posting {_lot, _amount} = M.insertWith (+) _lot _amount

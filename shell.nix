0f4f79c062a2a8a9d2f6571521e42500534befc9
Silvio Böhler <sboehler@noreply.users.github.com>
2018-04-26 00:39:59 +0200

    Update build files

--- shell.nix
+++ shell.nix
@@ -1,14 +1,34 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
 
 let
 
-  pkgs = nixpkgs.pkgs;
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, exceptions, filepath
+      , megaparsec, mtl, prettyprinter, scientific, stdenv, text, time
+      }:
+      mkDerivation {
+        pname = "haricot";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base containers exceptions filepath megaparsec mtl prettyprinter
+          scientific text time
+        ];
+        homepage = "https://github.com/sboehler/haricot#readme";
+        description = "A Haskell parser for beancount";
+        license = stdenv.lib.licenses.bsd3;
+      };
 
   haskellPackages = if compiler == "default"
                        then pkgs.haskellPackages
                        else pkgs.haskell.packages.${compiler};
 
-  drv = haskellPackages.callPackage (import ./default.nix) {};
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
 
 in
 

208a5023e9c7c7597cac192863ffb26daa0487f7
Silvio Böhler <sboehler@noreply.users.github.com>
2018-04-28 01:34:06 +0200

    Fix diverging division, use state monad

--- src/Haricot/Prices.hs
+++ src/Haricot/Prices.hs
@@ -6,10 +6,12 @@ module Haricot.Prices
   ) where
 
 import           Control.Exception
-import           Control.Monad.Catch(MonadThrow, throwM)
-import qualified Data.Map.Strict    as M
-import           Data.Scientific    (Scientific)
-import           Data.Time.Calendar (Day)
+import           Control.Monad.Catch        (MonadThrow, throwM)
+import           Control.Monad.State.Strict (MonadState, evalState, get, gets,
+                                             modify)
+import qualified Data.Map.Strict            as M
+import           Data.Scientific            (Scientific, toRealFloat, fromFloatDigits)
+import           Data.Time.Calendar         (Day)
 import           Haricot.AST
 import           Haricot.Ledger
 
@@ -23,29 +25,34 @@ data PriceException =
   deriving (Show)
 instance Exception PriceException
 
-calculatePrices :: Ledger -> PricesHistory
-calculatePrices l = fst $ foldl f (M.empty, M.empty) l
-  where
-    f (pricesHistory, prices) ts@Timestep {_date} =
-      let prices' = updatePrices ts prices
-       in (M.insert _date prices' pricesHistory, prices')
+calculatePrices :: Traversable t => t Timestep -> t Prices
+calculatePrices l = evalState (mapM updatePrices l) mempty
 
-updatePrices :: Timestep -> Prices -> Prices
-updatePrices Timestep {..} prices = foldr addPrice prices _prices
+updatePrices :: (MonadState Prices m) => Timestep -> m Prices
+updatePrices Timestep {_prices} =
+  mapM_ addPrice _prices  >>
+  mapM_ (addPrice . invert) _prices >>
+  get 
 
-addPrice :: Price -> Prices -> Prices
-addPrice Price {..} prices =
-  let p = M.findWithDefault M.empty _commodity prices
-      q = M.findWithDefault M.empty _targetCommodity prices
-      prices' = M.insert _commodity (M.insert _targetCommodity _price p) prices
-   in M.insert _targetCommodity (M.insert _commodity (1 / _price) q) prices'
+addPrice :: (MonadState Prices m) => Price -> m ()
+addPrice Price {..} = do
+  p <- gets (M.findWithDefault mempty _commodity)
+  modify $ M.insert _commodity (M.insert _targetCommodity _price p)
+
+invert :: Price -> Price
+invert p@Price {..} =
+  p
+    { _commodity = _targetCommodity
+    , _targetCommodity = _commodity
+    , _price = fromFloatDigits (1 / toRealFloat _price :: Double)
+    }
 
 getPrice :: MonadThrow m => Prices -> CommodityName -> CommodityName -> m Scientific
 getPrice prices source target =
   let v = find prices source target (M.fromList [(source, 1.0)]) []
    in case M.lookup target v of
         Just p -> return p
-        _ -> throwM $ NoPriceFound source target
+        _      -> throwM $ NoPriceFound source target
 
 find ::
      Prices

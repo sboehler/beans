ccc2caed6093a65b1956c0ec996f5df06db624ce
Silvio Böhler <sboehler@noreply.users.github.com>
2018-04-30 00:10:24 +0200

    Reorganize lib code

--- src/Haricot/Lib.hs
+++ src/Haricot/Lib.hs
@@ -4,38 +4,34 @@ module Haricot.Lib
 
 import           Control.Monad.Catch    (MonadThrow)
 import           Control.Monad.IO.Class (MonadIO)
-import           Control.Monad.Reader   (runReaderT)
 import           Control.Monad.Trans    (liftIO)
 import qualified Data.Map.Strict        as M
-import           Data.Scientific        (Scientific, formatScientific, FPFormat(Fixed))
-import           Data.Time.Calendar
-import           Haricot.Accounts
-import           Haricot.AST
-import           Haricot.Ledger
+import           Data.Scientific        (FPFormat (Fixed), Scientific,
+                                         formatScientific)
+import           Data.Time.Calendar     (fromGregorian)
+import           Haricot.Accounts       (Accounts, calculateAccounts,
+                                         mapWithKeys)
+import           Haricot.AST            (AccountName (..), CommodityName (..),
+                                         Lot)
+import           Haricot.Ledger         (buildLedger)
 import           Haricot.Parser         (parseFile)
-import           Haricot.Tabular
-import           Haricot.Valuation
+import           Haricot.Tabular        (ColDesc (..), left, right, showTable)
+import           Haricot.Valuation      (calculateValuation)
 import           System.Environment     (getArgs)
 
 
 parse :: (MonadIO m, MonadThrow m) => m ()
 parse = do
   (file:_) <- liftIO getArgs
-  ast <- parseFile file
-  let ledger = buildLedger ast
-  let valuationContext =
-        Config
-          (CommodityName "CHF")
-          (AccountName ["Equity", "Valuation"])
-  valuation <- runReaderT (calculateValuation ledger) valuationContext
+  ledger <- buildLedger <$> parseFile file
+  let target = CommodityName "CHF"
+      account = AccountName ["Equity", "Valuation"]
+  valuation <- calculateValuation target account ledger
   valHistory <- calculateAccounts valuation
-
   let accounts = M.lookupLE (fromGregorian 2017 12 9) valHistory
-  --accounts' = M.mapKeysWith mappend (const (AccountName ["a"])) . snd <$> accounts
-
   case accounts of
     Just (_, a) -> liftIO $ printAccounts a
-    Nothing     -> return()
+    Nothing     -> return ()
 
 data Row = Row {
   account   :: AccountName,

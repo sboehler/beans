503b2844e37513f5692874464022fbe6240af41d
Silvio Böhler <sboehler@noreply.users.github.com>
2018-05-01 13:56:06 +0200

    Function eraseLots

--- src/Haricot/Report/Balance.hs
+++ src/Haricot/Report/Balance.hs
@@ -1,14 +1,14 @@
 module Haricot.Report.Balance
   ( printAccounts
   , summarize
-  , combine
+  , eraseLots
   ) where
 
 import qualified Data.Map             as M
 import           Data.Scientific      (FPFormat (Fixed), Scientific,
                                        formatScientific)
-import           Haricot.Accounts     (Accounts, mapWithKeys, Holdings)
-import           Haricot.AST          (AccountName(..), CommodityName, Lot)
+import           Haricot.Accounts     (Accounts, mapWithKeys, Account(..))
+import           Haricot.AST          (AccountName(..), CommodityName, Lot(NoLot))
 import           Haricot.Report.Table (ColDesc (..), left, right, showTable)
 
 
@@ -24,9 +24,9 @@ printAccounts accounts =
   putStrLn $
   showTable
     [ ColDesc left "Account" left (show . account)
-    , ColDesc left "Lot" left (show . lot)
     , ColDesc left "Amount" right (formatScientific Fixed (Just 2) . amount)
     , ColDesc left "Commodity" left (show . commodity)
+    , ColDesc left "Lot" left (show . lot)
     ]
     (mapWithKeys Row accounts)
 
@@ -35,5 +35,6 @@ summarize depth  = M.mapKeysWith mappend m
   where
     m (AccountName l) = AccountName $ take depth l
 
-combine :: Holdings -> Holdings -> M.Map CommodityName Scientific
-h1 `combine` h2 = M.unionWith (+) (sum <$> h1) (sum <$> h2)
+eraseLots :: Account -> Account
+eraseLots Account {_holdings, ..} =
+  Account {_holdings = M.mapKeysWith (+) (const NoLot) <$> _holdings, ..}

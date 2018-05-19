ab1f07eee517a1cdb912b5ff45ac3ff9a04328a8
Silvio Böhler <sboehler@noreply.users.github.com>
2018-05-19 08:26:46 +0200

    Processing pipeline

--- src/Haricot/Lib.hs
+++ src/Haricot/Lib.hs
@@ -1,19 +1,21 @@
 module Haricot.Lib
-  ( parse
+  ( runHaricot
   , Options(..)
   , Command(..)
   ) where
 
 import           Control.Monad.Catch      (MonadThrow)
 import           Control.Monad.IO.Class   (MonadIO)
+import           Control.Monad.Reader     (MonadReader, asks, runReaderT)
 import           Control.Monad.Trans      (liftIO)
 import qualified Data.Map.Strict.Extended as M
 import           Data.Time.Calendar       (Day)
 import           Data.Time.LocalTime      (getZonedTime, localDay,
                                            zonedTimeToLocalTime)
-import           Haricot.Accounts         (calculateAccounts, diffAccounts)
+import           Haricot.Accounts         (Accounts, AccountsHistory,
+                                           calculateAccounts, diffAccounts)
 import           Haricot.AST              (AccountName (..), CommodityName (..))
-import           Haricot.Ledger           (buildLedger)
+import           Haricot.Ledger           (Ledger, buildLedger)
 import           Haricot.Parser           (parseFile)
 import           Haricot.Report.Balance   (eraseLots, printAccounts, summarize)
 import           Haricot.Valuation        (calculateValuation)
@@ -32,22 +34,43 @@ data Command =
   Balance
   deriving (Show)
 
-parse :: (MonadIO m, MonadThrow m) => Options -> m ()
-parse Options {..} = do
-  ledger <- buildLedger <$> parseFile optJournal
-  ledger' <-
+
+runHaricot :: (MonadIO m, MonadThrow m) => Options -> m ()
+runHaricot = runReaderT run
+
+run :: (MonadIO m, MonadThrow m, MonadReader Options m) =>  m ()
+run = do
+  report <- parseStage >>= valuationStage >>= accountsStage >>= reportStage >>= aggregationStage
+  liftIO $ printAccounts report
+
+parseStage :: (MonadIO m, MonadThrow m, MonadReader Options m) => m Ledger
+parseStage = buildLedger <$> (asks optJournal >>= parseFile)
+
+valuationStage :: (MonadIO m, MonadThrow m, MonadReader Options m) => Ledger -> m Ledger
+valuationStage ledger = do
     let account = AccountName ["Equity", "Valuation"]
-     in maybe pure (`calculateValuation` account) optMarket ledger
-  history <- calculateAccounts ledger'
-  to <- liftIO $ maybe getDate pure optTo
-  let accounts1 = M.lookupLessEqual to history
-      accounts0 = maybe mempty (`M.lookupLessEqual` history) optFrom
-  liftIO $
-    printAccounts $
-    (if optLots
-       then id
-       else eraseLots) <$>
-    maybe id summarize optDepth (accounts1 `diffAccounts` accounts0)
+    asks optMarket >>= \m -> maybe pure (`calculateValuation` account) m ledger
+
+accountsStage :: (MonadIO m, MonadThrow m, MonadReader Options m) => Ledger -> m AccountsHistory
+accountsStage = calculateAccounts
+
+reportStage :: (MonadIO m, MonadThrow m, MonadReader Options m) => AccountsHistory -> m Accounts
+reportStage h = asks optCommand >>= \case
+  Balance -> balanceReport h
+
+balanceReport :: (MonadIO m, MonadThrow m, MonadReader Options m) => AccountsHistory -> m Accounts
+balanceReport  accountsHistory = do
+  a1 <- asks optTo >>= liftIO . maybe getDate pure >>= \to -> pure $ M.lookupLessEqual to accountsHistory
+  a0 <- asks optFrom >>= \from -> pure $ maybe mempty (`M.lookupLessEqual` accountsHistory) from
+  return $ a1 `diffAccounts` a0
+
+aggregationStage :: (MonadIO m, MonadThrow m, MonadReader Options m) => Accounts -> m Accounts
+aggregationStage a = do
+  f1 <- (\case
+    True -> id
+    False -> eraseLots) <$> asks optLots
+  s <- asks optDepth >>= \n -> pure $ maybe id summarize n
+  return $ f1 <$> s a
 
 getDate :: IO Day
 getDate = localDay . zonedTimeToLocalTime <$> liftIO getZonedTime

2c021c498f4f2aac553866d046d1976784c4399e
Silvio Böhler <sboehler@noreply.users.github.com>
2018-09-22 21:40:55 +0200

    Separate check and book phase

--- src/Beans/Accounts.hs
+++ src/Beans/Accounts.hs
@@ -1,37 +1,33 @@
 module Beans.Accounts
   ( AccountsException(..)
   , Accounts
-  , State(..)
   , calculateAccounts
-  , updateAccounts
+  , processTimestep
   ) where
 
 import           Beans.Data.Accounts     (AccountName, Accounts, Amount,
                                           CommodityName)
 import qualified Beans.Data.Accounts     as A
 import           Beans.Data.Directives   (Balance (..), Close (..),
-                                          Command (..), Open (..), Posting,
+                                          Command (..), Open (..),
                                           Transaction (..))
 import qualified Beans.Data.Map          as M
 import           Beans.Data.Restrictions (Restriction, Restrictions)
 import qualified Beans.Data.Restrictions as R
-import           Beans.Ledger            (Ledger, Timestep (..), toList)
+import           Beans.Ledger            (Ledger (..), Timestep (..))
 import           Control.Monad           (unless, when)
 import           Control.Monad.Catch     (Exception, MonadThrow, throwM)
-import           Control.Monad.State     (MonadState, evalStateT, get, gets,
-                                          modify)
+import           Control.Monad.State     (MonadState, evalStateT, get, modify,
+                                          put)
 import           Data.Time.Calendar      (Day)
-
-data State = S
-  { sAccounts     :: Accounts
-  , sRestrictions :: Restrictions
-  , sCheck        :: Bool
-  }
 
 data AccountsException
   = AccountIsNotOpen Close
   | BookingErrorAccountNotOpen AccountName
-  | BookingErrorCommodityIncompatible AccountName CommodityName Amount Restriction
+  | BookingErrorCommodityIncompatible AccountName
+                                      CommodityName
+                                      Amount
+                                      Restriction
   | AccountIsAlreadyOpen Open
   | BalanceIsNotZero Close
   | AccountDoesNotExist Balance
@@ -41,51 +37,61 @@ data AccountsException
 
 instance Exception AccountsException
 
-calculateAccounts :: (MonadThrow m) => Bool -> Ledger -> m (M.Map Day Accounts)
-calculateAccounts check l =
-  let computation = M.fromListM <$> mapM updateAccounts (toList l)
-   in evalStateT computation (S mempty mempty check)
+calculateAccounts :: (MonadThrow m) => Ledger -> m (M.Map Day Accounts)
+calculateAccounts (Ledger l) =
+  let
+    processing = M.fromListM <$> mapM processTimestep l
+   in do
+    evalStateT (mapM_ checkTimestep l) mempty
+    evalStateT processing mempty
 
-updateAccounts ::
-     (MonadThrow m, MonadState State m) => Timestep -> m (Day, Accounts)
-updateAccounts (Timestep day commands) = do
-  a <- mapM_ process commands >> gets sAccounts
+processTimestep ::
+     (MonadThrow m, MonadState Accounts m) => Timestep -> m (Day, Accounts)
+processTimestep (Timestep day commands) = do
+  a <- mapM_ process commands >> get
   return (day, a)
 
-process :: (MonadThrow m, MonadState State m) => Command -> m ()
-process (OpenCommand open@Open {oAccount, oRestriction}) = do
-  S {sRestrictions} <- get
-  when (oAccount `M.member` sRestrictions) (throwM $ AccountIsAlreadyOpen open)
-  modify (\s -> s {sRestrictions = M.insert oAccount oRestriction sRestrictions})
-process (CloseCommand closing@Close {cAccount}) = do
-  (restriction, remainingRestrictions) <- M.partitionWithKey (const . (==  cAccount)) <$> gets sRestrictions
-  when (null restriction) (throwM $ AccountIsNotOpen closing)
-  (deletedAccount, remainingAccounts) <- M.partitionWithKey g <$> gets sAccounts
-  unless (all (== 0) deletedAccount) (throwM $ BalanceIsNotZero closing)
-  modify
-    (\s ->
-       s {sRestrictions = remainingRestrictions, sAccounts = remainingAccounts})
-    where
-      g (a, _, _) _ = a == cAccount
-process (BalanceCommand bal@Balance {bAccount, bAmount, bCommodity}) = do
-  check <- gets sCheck
-  when check $ do
-    r <- gets sRestrictions
-    unless (bAccount `M.member` r) (throwM $ AccountDoesNotExist bal)
-    s <- A.balance bAccount bCommodity <$> gets sAccounts
-    unless (s == bAmount) (throwM $ BalanceDoesNotMatch bal s)
-process (TransactionCommand Transaction {tPostings}) =
-  mapM_ bookPosting $ M.toList tPostings
-process _ = pure ()
+checkTimestep :: (MonadThrow m, MonadState Restrictions m) => Timestep -> m ()
+checkTimestep (Timestep _ commands) = mapM_ check commands
 
-bookPosting :: (MonadThrow m, MonadState State m) => Posting -> m ()
-bookPosting ((account, commodity, lot), amount) = do
-  S {sRestrictions, sAccounts} <- get
-  case M.lookup account sRestrictions of
-    Nothing -> throwM $ BookingErrorAccountNotOpen account
-    Just r -> do
-      unless
-        (R.isCompatible r commodity)
-        (throwM $ BookingErrorCommodityIncompatible account commodity amount r)
-      modify
-        (\s -> s {sAccounts = M.insertM (account, commodity, lot) amount sAccounts})
+
+check :: (MonadThrow m, MonadState Restrictions m) => Command -> m ()
+check (OpenCommand open@Open {oAccount, oRestriction}) = do
+  restrictions <- get
+  when (oAccount `M.member` restrictions) (throwM $ AccountIsAlreadyOpen open)
+  modify $ M.insert oAccount oRestriction
+check (CloseCommand closing@Close {cAccount}) = do
+  (restriction, remainingRestrictions) <-
+    M.partitionWithKey (const . (== cAccount)) <$> get
+  when (null restriction) (throwM $ AccountIsNotOpen closing)
+  put remainingRestrictions
+check (TransactionCommand Transaction {tPostings}) = do
+  restrictions <- get
+  mapM_ (g restrictions) $ M.toList tPostings
+  where
+    g restrictions ((a, c, _), s) =
+      case M.lookup a restrictions of
+        Nothing -> throwM $ BookingErrorAccountNotOpen a
+        Just r  ->
+          unless
+            (R.isCompatible r c)
+            (throwM $ BookingErrorCommodityIncompatible a c s r)
+check (BalanceCommand bal@Balance {bAccount}) = do
+  r <- get
+  unless (bAccount `M.member` r) (throwM $ AccountDoesNotExist bal)
+check _ = pure ()
+
+
+
+process :: (MonadThrow m, MonadState Accounts m) => Command -> m ()
+process (CloseCommand closing@Close {cAccount}) = do
+  (deletedAccount, remainingAccounts) <- M.partitionWithKey g <$> get
+  unless (all (== 0) deletedAccount) (throwM $ BalanceIsNotZero closing)
+  put remainingAccounts
+   where
+    g (a, _, _) _ = a == cAccount
+process (TransactionCommand Transaction {tPostings}) = modify $ mappend tPostings
+process (BalanceCommand bal@Balance {bAccount, bAmount, bCommodity}) = do
+  s <- A.balance bAccount bCommodity <$> get
+  unless (s == bAmount) (throwM $ BalanceDoesNotMatch bal s)
+process _ = pure ()

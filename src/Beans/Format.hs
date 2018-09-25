49d0e84d211983784c33c5f0b3bf040872dc47a7
Silvio Böhler <sboehler@noreply.users.github.com>
2018-09-25 22:32:20 +0200

    Simplify report

--- src/Beans/Format.hs
+++ src/Beans/Format.hs
@@ -31,9 +31,7 @@ data Section = Section
 createHierarchicalReport :: Accounts -> Section
 createHierarchicalReport = groupSections . fmap toItem . M.toList
   where
-    toItem ((a, c, l), s) =
-      let pos = M.singleton (c, l) s
-       in (toLabel a, pos)
+    toItem ((a, c, l), s) = (toLabel a, M.singleton (c, l) s)
     toLabel (AccountName t ns) = T.pack (show t) : ns
 
 groupSections :: [([Text], Positions)] -> Section
@@ -67,48 +65,36 @@ groupSections2 items =
 
 -- Formatting a report into rows
 data Row = Row
-  { rAccount        :: Text
-  , rTotal          :: Text
-  , rTotalCommodity :: Text
-  , rAmount         :: Text
-  , rCommodity      :: Text
+  { rAccount   :: Text
+  , rAmount    :: Text
+  , rCommodity :: Text
   }
 
 reportToRows :: Section -> [Row]
 reportToRows t = sectionToRows ("", t)
 
 sectionToRows :: (Text, Section) -> [Row]
-sectionToRows (t, Section ps ss st) = positions ++ subsections
+sectionToRows (label, Section _ subsections subtotals) =
+  positionRows ++ subsectionRows
   where
-    subsections = indent 2 <$> (sectionToRows =<< M.toList ss)
-    subtotals =
-      case subsections of
-        [] -> mempty
-        _  -> st
-    positions = positionsToRows t ps subtotals
+    subsectionRows = indent 2 <$> (sectionToRows =<< M.toList subsections)
+    positionRows = positionsToRows label subtotals
 
-positionsToRows :: Text -> Positions -> Positions -> [Row]
-positionsToRows title positions subtotals =
+positionsToRows :: Text -> Positions -> [Row]
+positionsToRows title subtotals =
   let nbrSubtotals = M.size subtotals
-      nbrPositions = M.size positions
-      nbrRows = maximum [1, nbrSubtotals, nbrPositions]
+      nbrRows = maximum [1, nbrSubtotals]
       st =
         flattenPositions subtotals ++
         replicate (nbrRows - nbrSubtotals) (Nothing, Nothing, Nothing)
-      ps =
-        flattenPositions positions ++
-        replicate (nbrRows - nbrPositions) (Nothing, Nothing, Nothing)
-   in do (rAccount, (_, rTotalCommodity, rTotalAmount), (lot, commodity, rAmount)) <-
-           L.zip3 (title : repeat "") st ps
+   in do (rAccount, (lot, commodity, amount)) <- zip (title : repeat "") st
          pure
            Row
              { rAccount
-             , rTotal = maybe "" formatStandard rTotalAmount
-             , rTotalCommodity = T.pack $ maybe "" show rTotalCommodity
-             , rAmount = maybe "" formatStandard rAmount
+             , rAmount = maybe "" formatStandard amount
              , rCommodity =
-                 T.pack (maybe "" show commodity) <>
-                 T.pack (maybe "" (show . pretty) lot)
+                 T.pack $
+                 unwords [maybe "" show commodity, maybe "" (show . pretty) lot]
              }
 
 flattenPositions ::
@@ -119,7 +105,7 @@ flattenPositions positions = do
   return (lot, Just commodity, Just amount)
 
 indent :: Int -> Row -> Row
-indent n (Row first a b c d) = Row (indent' first) a b c d
+indent n (Row first a b) = Row (indent' first) a b
   where
     indent' t = T.replicate (fromIntegral n) " " <> t
 
@@ -128,8 +114,6 @@ formatTable :: [Row] -> Text
 formatTable =
   showTable
     [ Column left "Account" left rAccount
-    , Column left "Total" right rTotal
-    , Column left "Commodity" left rTotalCommodity
     , Column left "Amount" right rAmount
     , Column left "Commodity" left rCommodity
     ]

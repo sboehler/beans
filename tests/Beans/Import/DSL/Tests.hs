2f9a293cd8c1c64d182b8d4bed320b248b18c8f2
Silvio Böhler <sboehler@noreply.users.github.com>
2018-09-28 15:55:25 +0200

    Add test cases for evaluation

--- tests/Beans/Import/DSL/Tests.hs
+++ tests/Beans/Import/DSL/Tests.hs
@@ -1,60 +1,106 @@
 module Beans.Import.DSL.Tests where
 
+import           Beans.Data.Accounts        (AccountName (..), AccountType (..))
+import           Beans.Import.Common        (Entry (..))
 import           Beans.Import.DSL
 import qualified Data.ByteString.Lazy.Char8 as BS
+import           Data.Semigroup             ((<>))
 import           Data.Text                  (Text)
+import           Data.Time.Calendar         (fromGregorian)
 import           System.FilePath.Posix      ((<.>), (</>))
 import           Test.Tasty                 (TestTree, testGroup)
 import           Test.Tasty.Golden          (goldenVsString)
+import           Test.Tasty.HUnit           (assertEqual, testCase)
 import           Text.Megaparsec            (parse, parseErrorPretty)
 
-mkTest :: (Show a) => String -> Parser a -> Text -> TestTree
-mkTest testName parser input = goldenVsString testName fileName action
+mkParserTest :: (Show a) => String -> Parser a -> Text -> TestTree
+mkParserTest testName parser input = goldenVsString testName fileName action
   where
     fileName = "tests/Beans/Import/DSL" </> testName <.> "golden"
     action =
       (return . BS.pack . either parseErrorPretty show . parse parser testName)
         input
 
-mkGroup :: (Show a) => String -> Parser a -> [(Int, Text)] -> TestTree
-mkGroup groupName parser cases =
+mkParserTestGroup :: (Show a) => String -> Parser a -> [(Int, Text)] -> TestTree
+mkParserTestGroup groupName parser cases =
   testGroup
     groupName
-    [mkTest (groupName ++ show n) parser input | (n, input) <- cases]
+    [mkParserTest (groupName ++ show n) parser input | (n, input) <- cases]
 
 tests :: TestTree
-tests =
-  testGroup "Beans.DSL.Parser" [boolExprTests, amountExprTests, rulesTests]
+tests = testGroup "DSL" [parserTests, evaluationTests]
 
-boolExprTests :: TestTree
-boolExprTests =
-  mkGroup
-    "boolExpr"
-    boolExpr
-    [ (1, "true")
-    , (2, "false")
-    , (3, "(true)")
-    , (4, "(valueDate > 2018-01-01) and (valueDate\n <= \n2018-10-01)")
-    , (5, "(amount <= 4.0) and (amount >= 2) or (40 < 50)")
-    , (6, "description ~=~ \"asdf.*\"")
-    , (7, "not true or false and not false")
-    ]
+parserTests :: TestTree
+parserTests =
+  testGroup
+    "Beans.DSL.Parser"
+    [boolExprTests, amountExprTests, rulesTests, textExprTests]
+  where
+    boolExprTests =
+      mkParserTestGroup
+        "boolExpr"
+        boolExpr
+        [ (1, "true")
+        , (2, "false")
+        , (3, "(true)")
+        , (4, "(valueDate > 2018-01-01) and (valueDate\n <= \n2018-10-01)")
+        , (5, "(amount <= 4.0) and (amount >= 2) or (40 < 50)")
+        , (6, "description ~=~ \"asdf.*\"")
+        , (7, "not true or false and not false")
+        ]
+    amountExprTests =
+      mkParserTestGroup
+        "amountExpr"
+        amountExpr
+        [ (1, "amount")
+        , (2, "4")
+        , (3, "(4.0)")
+        , (4, "((amount + 4) - 4.0)")
+        , (5, "(amount - amount + amount)")
+        ]
+    textExprTests =
+      mkParserTestGroup
+        "textExpr"
+        textExpr
+        [ (1, "\"some text\"")
+        , (2, "(\"some text within parens\")")
+        , (3, "description")
+        ]
+    rulesTests =
+      mkParserTestGroup
+        "rules"
+        rules
+        [(1, "true-> Assets:Account;"), (2, "false -> Assets:Account;")]
 
-amountExprTests :: TestTree
-amountExprTests =
-  mkGroup
-    "amountExpr"
-    amountExpr
-    [ (1, "amount")
-    , (2, "4")
-    , (3, "(4.0)")
-    , (4, "((amount + 4) - 4.0)")
-    , (5, "(amount - amount + amount)")
-    ]
-
-rulesTests :: TestTree
-rulesTests =
-  mkGroup
-    "rules"
-    rules
-    [(1, "true-> Assets:Account;"), (2, "false-> Assets:Account;")]
+evaluationTests :: TestTree
+evaluationTests = testGroup "evaluation tests" (mkTest <$> evaluationTestCases)
+  where
+    entry =
+      Entry
+        (fromGregorian 2018 1 1)
+        "Purchasing Foos at Bar Inc"
+        (-100)
+        (fromGregorian 2018 1 1)
+        (Just 500)
+    account1 = AccountName Expenses ["Shopping"]
+    account2 = AccountName Assets ["Bankaccount"]
+    account3 = AccountName Liabilities ["Creditcard"]
+    mkTest (rs, e) =
+      testCase (show rs) $
+      assertEqual (" should evaluate to " <> show e) e (evaluate rs entry)
+    evaluationTestCases =
+      [ ( [Rule (EMatch EVarDescription (EText "Bar Inc")) account1]
+        , Just account1)
+      , ([Rule (EBool True) account2], Just account2)
+      , ([Rule (EGT EVarAmount (EAmount (-110))) account3], Just account3)
+      , ([Rule (ELT EVarAmount (EAmount (-110))) account3], Nothing)
+      , ([Rule (EEQ EVarDescription (EText "Purchasing Foos at Bar Inc")) account3], Just account3)
+      , ([Rule (EMatch EVarDescription (EText "^Purchasing")) account3], Just account3)
+      , ([Rule (EMatch EVarDescription (EText "^Foo")) account3], Nothing)
+      , ([Rule (EOr (EBool True) (EBool False)) account3], Just account3)
+      , ([Rule (EOr (EBool False) (EBool False)) account3], Nothing)
+      , ([Rule (EOr (EBool False) (EBool True)) account3], Just account3)
+      , ([Rule (EOr (EBool True) (EBool True)) account3], Just account3)
+      , ([Rule (EGT EVarBookingDate (EDate $ fromGregorian 2017 12 31)) account1], Just account1)
+      , ([Rule (ELT EVarBookingDate (EDate $ fromGregorian 2017 12 31)) account1], Nothing)
+      ]

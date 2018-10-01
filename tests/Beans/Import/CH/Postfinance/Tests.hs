793118085554334bbc620f3166372f5205e57355
Silvio Böhler <sboehler@noreply.users.github.com>
2018-10-01 23:39:32 +0200

    Add test for Postfinance importer

---
+++ tests/Beans/Import/CH/Postfinance/Tests.hs
@@ -0,0 +1,27 @@
+module           Beans.Import.CH.Postfinance.Tests (tests)
+  where
+
+import           Beans.Import.CH.Postfinance (name, parseEntries)
+import qualified Data.ByteString.Lazy.Char8  as BS
+import           Data.Text                   (pack)
+import           System.FilePath.Posix       ((</>))
+import           Test.Tasty                  (TestTree, testGroup)
+import           Test.Tasty.Golden           (goldenVsString)
+import           Test.Tasty.HUnit            (assertEqual, testCase)
+
+tests :: TestTree
+tests = testGroup "CH.Postfinance" [test1, test2]
+
+test1 :: TestTree
+test1 = goldenVsString "Postfinance" goldenFile action
+ where
+  path       = "tests/Beans/Import/CH/Postfinance/"
+  sourceFile = path </> "postfinance1.csv"
+  goldenFile = path </> "postfinance1.golden"
+  action     = BS.pack . unlines . fmap show <$> parseEntries sourceFile
+
+test2 :: TestTree
+test2 = testCase "name attribute" $ assertEqual
+  "Correct name"
+  Beans.Import.CH.Postfinance.name
+  (pack "ch.postfinance")

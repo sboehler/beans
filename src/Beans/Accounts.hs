fc26b248524c5c8a4b3c76a0eb744ae6de2c23ff
Silvio Böhler <sboehler@noreply.users.github.com>
2018-10-11 21:12:35 +0200

    Use folds

--- src/Beans/Accounts.hs
+++ src/Beans/Accounts.hs
@@ -39,13 +39,6 @@ import           Control.Monad.Catch                      ( Exception
                                                           , MonadThrow
                                                           , throwM
                                                           )
-import           Control.Monad.State                      ( MonadState
-                                                          , evalStateT
-                                                          , get
-                                                          , gets
-                                                          , modify
-                                                          , put
-                                                          )
 
 data AccountsException
   = AccountIsNotOpen Close
@@ -77,47 +70,48 @@ calculateAccountsForDays _ [] _ = return []
 
 checkTimestep :: (MonadThrow m) => Restrictions -> Timestep -> m Restrictions
 checkTimestep restrictions (Timestep _ commands) =
-  evalStateT (mapM_ check commands >> get) restrictions
+  foldM check restrictions commands
 
-
-check :: (MonadThrow m, MonadState Restrictions m) => Command -> m ()
-check (OpenCommand open@Open { oAccount, oRestriction }) = do
-  restrictions <- get
+check :: (MonadThrow m) => Restrictions -> Command -> m Restrictions
+check restrictions (OpenCommand open@Open { oAccount, oRestriction }) = do
   when (oAccount `M.member` restrictions) (throwM $ AccountIsAlreadyOpen open)
-  modify $ M.insert oAccount oRestriction
-check (CloseCommand closing@Close { cAccount }) = do
-  (restriction, remainingRestrictions) <- gets
-    (M.partitionWithKey (const . (== cAccount)))
+  return $ M.insert oAccount oRestriction restrictions
+check restrictions (CloseCommand closing@Close { cAccount }) = do
+  let (restriction, remainingRestrictions) =
+        M.partitionWithKey (const . (== cAccount)) restrictions
   when (null restriction) (throwM $ AccountIsNotOpen closing)
-  put remainingRestrictions
-check (TransactionCommand Transaction { tPostings }) = do
-  restrictions <- get
+  return remainingRestrictions
+check restrictions (TransactionCommand Transaction { tPostings }) = do
   mapM_ (g restrictions) $ M.toList tPostings
+  return restrictions
  where
-  g restrictions ((a, c, _), s) = case M.lookup a restrictions of
+  g r ((a, c, _), s) = case M.lookup a r of
     Nothing -> throwM $ BookingErrorAccountNotOpen a
-    Just r  -> unless
-      (R.isCompatible r c)
-      (throwM $ BookingErrorCommodityIncompatible a c (M.findWithDefaultM c s) r
+    Just r' -> unless
+      (R.isCompatible r' c)
+      ( throwM
+      $ BookingErrorCommodityIncompatible a c (M.findWithDefaultM c s) r'
       )
-check (BalanceCommand bal@Balance { bAccount }) = do
-  r <- get
-  unless (bAccount `M.member` r) (throwM $ AccountDoesNotExist bal)
-check _ = pure ()
+check restrictions (BalanceCommand bal@Balance { bAccount }) = do
+  unless (bAccount `M.member` restrictions) $ throwM (AccountDoesNotExist bal)
+  return restrictions
+check restrictions _ = pure restrictions
 
 processTimestep :: (MonadThrow m) => Accounts -> Timestep -> m Accounts
 processTimestep accounts (Timestep _ commands) =
-  evalStateT (mapM_ process commands >> get) accounts
+  foldM process accounts commands
 
-process :: (MonadThrow m, MonadState Accounts m) => Command -> m ()
-process (CloseCommand closing@Close { cAccount }) = do
-  (deletedAccount, remainingAccounts) <- gets (M.partitionWithKey g)
+process :: (MonadThrow m) => Accounts -> Command -> m Accounts
+process accounts (CloseCommand closing@Close { cAccount }) = do
+  let (deletedAccount, remainingAccounts) = M.partitionWithKey g accounts
   unless ((all . all) (== 0) deletedAccount) (throwM $ BalanceIsNotZero closing)
-  put remainingAccounts
+  return remainingAccounts
   where g (a, _, _) _ = a == cAccount
-process (TransactionCommand Transaction { tPostings }) =
-  modify $ mappend tPostings
-process (BalanceCommand bal@Balance { bAccount, bAmount, bCommodity }) = do
-  s <- gets $ A.balance bAccount bCommodity
-  unless (s == bAmount) (throwM $ BalanceDoesNotMatch bal s)
-process _ = pure ()
+process accounts (TransactionCommand Transaction { tPostings }) =
+  return $ accounts `mappend` tPostings
+process accounts (BalanceCommand bal@Balance { bAccount, bAmount, bCommodity })
+  = do
+    let s = A.balance bAccount bCommodity accounts
+    unless (s == bAmount) (throwM $ BalanceDoesNotMatch bal s)
+    return accounts
+process accounts _ = pure accounts

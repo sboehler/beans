6a765075897a1b5cbd4175923cbce5496811e0ea
Silvio Böhler <sboehler@noreply.users.github.com>
2018-11-25 22:53:37 +0100

    Generalize number & date parsing
    At some point, those parsers should be unified across beans.

--- src/Beans/Import/CH/SupercardPlus.hs
+++ src/Beans/Import/CH/SupercardPlus.hs
@@ -4,9 +4,11 @@ module Beans.Import.CH.SupercardPlus
   )
 where
 
+import           Data.Void                      ( Void )
 import           Data.Monoid                    ( Sum(Sum) )
 import           Beans.Import.Common            ( Config(..)
                                                 , Context(..)
+                                                , ParserException(ParseError)
                                                 , Parser
                                                 , askAccount
                                                 , parseUtf8
@@ -16,10 +18,10 @@ import           Beans.Model                    ( Amount
                                                 , Command(Transaction)
                                                 , Commodity(Commodity)
                                                 , Date
+                                                , parseDate
                                                 , Dated(Dated)
                                                 , Flag(Complete)
                                                 , Position(Position)
-                                                , fromGreg
                                                 )
 import           Control.Monad                  ( void )
 import           Control.Monad.Catch            ( MonadThrow )
@@ -27,54 +29,53 @@ import           Control.Monad.IO.Class         ( MonadIO )
 import           Control.Monad.Reader           ( MonadReader
                                                 , asks
                                                 )
-import           Data.Group                     ( invert )
 import           Data.Text                      ( Text )
 import qualified Data.Text                     as T
 import           Data.Char                      ( isDigit
                                                 , isUpper
                                                 )
 import           Text.Megaparsec                ( (<|>)
-                                                , count
-                                                , manyTill
+                                                , between
                                                 , skipManyTill
                                                 , many
-                                                , manyTill
+                                                , customFailure
                                                 , takeWhile1P
                                                 , takeWhileP
-                                                , optional
+                                                , option
                                                 , eof
+                                                , Parsec
                                                 , sepEndBy
+                                                , parseMaybe
                                                 )
 import           Text.Megaparsec.Char           ( anyChar
-                                                , string
                                                 , char
                                                 , upperChar
                                                 , digitChar
                                                 , eol
+                                                , string
                                                 )
 import qualified Text.Megaparsec.Char.Lexer    as L
+import qualified Data.List                     as List
 
 
 name :: T.Text
 name = "ch.supercardplus"
 
 parse :: (MonadIO m, MonadThrow m, MonadReader Config m) => m [Dated Command]
-parse = parseUtf8 $ ignoreLine >> command `sepEndBy` eol <* eof
+parse = List.sort <$> parseUtf8 (ignoreLine >> command `sepEndBy` eol <* eof)
 
 command :: Parser (Dated Command)
 command = do
-  date              <- dateField <* dateField
-  card              <- field $ takeWhile1P (Just "Card Number") isDigit
-  sign              <- optional $ string "-"
-  commodity         <- Commodity <$> takeWhile1P (Just "Currency") isUpper
-  amount            <- amountField
-  description       <- quotedField
-  location          <- quotedField
-  country           <- textField "country"
-  zipCode           <- textField "zip code"
-  transactionNumber <- quotedField
-  transactionType   <- field upperChar <* many digitChar
-  account           <- asks cAccount
+  date                <- dateField <* dateField
+  card                <- field $ takeWhile1P (Just "Card Number") isDigit
+  (commodity, amount) <- amountField
+  description         <- quotedField
+  location            <- quotedField
+  country             <- textField "Merchant Cuntry"
+  zipCode             <- textField "Merchant Zip Code"
+  transactionNumber   <- quotedField
+  transactionType     <- field upperChar <* many digitChar
+  account             <- asks cAccount
   let desc = T.unwords
         [ card
         , description
@@ -84,13 +85,7 @@ command = do
         , transactionNumber
         , T.singleton transactionType
         ]
-  otherAccount <- askAccount $ Context
-    date
-    "expense"
-    desc
-    (if null sign then amount else invert amount)
-    commodity
-    name
+  otherAccount <- askAccount $ Context date "expense" desc amount commodity name
   let bookings = M.fromListM
         [ (Position account commodity Nothing, M.singleton commodity amount)
         , ( Position otherAccount commodity Nothing
@@ -101,21 +96,27 @@ command = do
 
 dateField :: Parser Date
 dateField = do
-  day   <- int 2 <* dot
-  month <- int 2 <* dot
-  year  <- int 4 <* comma
-  return $ fromGreg year month day
- where
-  comma = char ','
-  dot   = char '.'
-  int n = read <$> count n digitChar
+  inp <- T.unpack <$> textField "Date"
+  case parseDate "%-d.%-m.%Y" inp of
+    Just d  -> return d
+    Nothing -> customFailure $ ParseError $ unwords ["Invalid date:", inp]
 
-amountField :: Parser Amount
-amountField = Sum . read . filter (/= '\'') <$> manyTill anyChar (char ',')
+amountField :: Parser (Commodity, Amount)
+amountField = do
+  sign      <- option mempty $ string "-"
+  commodity <- Commodity <$> takeWhile1P (Just "Currency") isUpper
+  number    <- T.filter (/= '\'') <$> textField "Amount"
+  case parseMaybe amountP (sign <> number) of
+    Just amount -> return (commodity, amount)
+    Nothing     -> customFailure $ ParseError $ unwords
+      ["Invalid amount:", T.unpack $ sign <> number]
 
+amountP :: Parsec Void Text Amount
+amountP = Sum <$> L.signed (pure ()) L.scientific
 
 quotedField :: Parser Text
-quotedField = quote >> T.pack <$> manyTill anyChar (quote >> separator)
+quotedField = field
+  $ between quote quote (takeWhileP (Just "quoted string") (/= '"'))
   where quote = char '"'
 
 textField :: String -> Parser Text

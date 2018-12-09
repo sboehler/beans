1de772bf4eec566a37ae9019d2e311cb2bfb570b
Silvio Böhler <sboehler@noreply.users.github.com>
2018-12-09 01:00:02 +0100

    Use hpack

--- default.nix
+++ default.nix
@@ -1,26 +1,29 @@
 { mkDerivation, base, bifunctors, bytestring, containers
-, exceptions, filepath, groups, megaparsec, mtl
+, exceptions, filepath, groups, hpack, megaparsec, mtl
 , optparse-applicative, pretty-simple, prettyprinter, regex-pcre
 , scientific, stdenv, tasty, tasty-golden, tasty-hunit
 , tasty-quickcheck, tasty-smallcheck, text, time
 }:
 mkDerivation {
   pname = "beans";
-  version = "0.1.0.0";
+  version = "0.0.2.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     base bifunctors bytestring containers exceptions filepath groups
-    megaparsec mtl optparse-applicative pretty-simple prettyprinter
-    regex-pcre scientific text time
+    megaparsec mtl prettyprinter regex-pcre scientific text time
   ];
-  executableHaskellDepends = [ base optparse-applicative ];
+  libraryToolDepends = [ hpack ];
+  executableHaskellDepends = [
+    base megaparsec optparse-applicative text time
+  ];
   testHaskellDepends = [
-    base bytestring filepath megaparsec mtl regex-pcre tasty
-    tasty-golden tasty-hunit tasty-quickcheck tasty-smallcheck text
-    time
+    base bytestring filepath megaparsec mtl pretty-simple prettyprinter
+    regex-pcre tasty tasty-golden tasty-hunit tasty-quickcheck
+    tasty-smallcheck text time
   ];
+  preConfigure = "hpack";
   homepage = "https://github.com/sboehler/beans#readme";
   description = "A plain text accounting tool";
   license = stdenv.lib.licenses.bsd3;

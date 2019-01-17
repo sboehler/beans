ffbf18e26dc684f926a80afec9695b2578677bc5
Silvio Böhler <sboehler@noreply.users.github.com>
2019-01-17 01:02:38 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,28 +1,33 @@
 { mkDerivation, base, bifunctors, bytestring, containers, Decimal
 , exceptions, filepath, groups, hpack, lens, megaparsec, mtl
-, optparse-applicative, pretty-simple, prettyprinter, regex-pcre
-, scientific, stdenv, tasty, tasty-golden, tasty-hunit
+, optparse-applicative, parser-combinators, prettyprinter
+, regex-pcre, scientific, stdenv, tasty, tasty-golden, tasty-hunit
 , tasty-quickcheck, tasty-smallcheck, text, time
 }:
 mkDerivation {
   pname = "beans";
-  version = "0.0.2.0";
+  version = "0.0.3.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     base bifunctors bytestring containers Decimal exceptions filepath
-    groups lens megaparsec mtl prettyprinter regex-pcre scientific text
-    time
+    groups lens megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-smallcheck text time
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    base megaparsec optparse-applicative text time
+    base bifunctors bytestring containers Decimal exceptions filepath
+    groups lens megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-smallcheck text time
   ];
   testHaskellDepends = [
-    base bytestring filepath megaparsec mtl pretty-simple prettyprinter
-    regex-pcre tasty tasty-golden tasty-hunit tasty-quickcheck
-    tasty-smallcheck text time
+    base bifunctors bytestring containers Decimal exceptions filepath
+    groups lens megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-smallcheck text time
   ];
   preConfigure = "hpack";
   homepage = "https://github.com/sboehler/beans#readme";

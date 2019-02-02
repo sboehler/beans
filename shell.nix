32ca3ec94ec5709ed22f15f2502453f092eec336
Silvio Böhler <sboehler@noreply.users.github.com>
2019-02-02 17:30:04 +0100

    Improve shell.nix

--- shell.nix
+++ shell.nix
@@ -4,19 +4,39 @@
     url = "https://github.com/NixOS/nixpkgs/archive/44b02b52ea6a49674f124f50009299f192ed78bb.tar.gz";
     sha256 = "0gmk6w1lnp6kjf26ak8jzj0h2qrnk7bin54gq68w1ky2pdijnc44";
   }) {},
-
-  # My system wide nix snapshot, usually tracking nixos-unstable
-  unstable ? import <nixpkgs> {},
   compiler ? "ghc863",
-  withHoogle ? true
 }:
 
 let
+  config = {
 
-  inherit (unstable) pkgs;
+    packageOverrides = pkgs: rec {
+
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+        ${compiler} = pkgs.haskell.packages.${compiler}.override {
+            overrides = self: super: {
+              ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+              ghcWithPackages = self.ghc.withPackages;
+            };
+          };
+        };
+      };
+      profiledHaskellPackages = pkgs.haskell.${compiler}.override {
+        overrides = self: super: {
+          mkDerivation = args: super.mkDerivation (args // {
+            enableLibraryProfiling = false;
+          });
+        };
+      };
+    };
+  };
+
+  pkgs = import <nixpkgs> { inherit config; };
 
   f = import ./default.nix;
 
+  # haskellPackages = pkgs.profiledHaskellPackages; # pkgs.haskell.packages.${compiler};
   haskellPackages = pkgs.haskell.packages.${compiler};
 
   # Haskell IDE Engine
@@ -28,21 +48,9 @@ let
     rev = "19f47e0bf2e2f1a793bf87d64bf8266062f422b1";
   }) {}).hies;
 
-  hspkgs = (
-      if withHoogle then
-        haskellPackages.override {
-          overrides = (self: super: {
-            ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
-            ghcWithPackages = self.ghc.withPackages;
-          });
-        }
-        else haskellPackages
-    );
+  drv = haskellPackages.callPackage f {};
 
-
-  drv = hspkgs.callPackage f {};
-
-  drvWithTools = pkgs.haskell.lib.addBuildDepends drv (with hspkgs; [
+  drvWithTools = pkgs.haskell.lib.addBuildDepends drv (with haskellPackages; [
     hies
     ghcid
     cabal-install

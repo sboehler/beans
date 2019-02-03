852f32e6f5786a650ebfbddf840dbfd9b1c371d2
Silvio Böhler <sboehler@noreply.users.github.com>
2019-02-03 12:31:10 +0100

    Refactor showRounded

--- src/Data/Fixed/Extended.hs
+++ src/Data/Fixed/Extended.hs
@@ -7,29 +7,20 @@ where
 
 import           Data.Fixed
 
-
--- only works for positive a
-showIntegerZeros :: Int -> Integer -> String
-showIntegerZeros n      0 = replicate n '0'
-showIntegerZeros digits a = replicate (digits - length s) '0' ++ s
-  where s = show a
-
-withDot :: String -> String
-withDot "" = ""
-withDot s  = '.' : s
-
-showRounded :: (HasResolution a) => Int -> Fixed a -> String
-showRounded digits fa = sign ++ show i ++ withDot
-  (take digits $ showIntegerZeros digits' d)
- where
-  sign      = if fa < 0 then "-" else ""
-  MkFixed a = roundTo digits fa
-  res       = resolution fa
-  (i, d)    = divMod (abs a) res
-  digits'   = ceiling (logBase 10 (fromInteger res) :: Double)
-
 roundTo :: HasResolution a => Int -> Fixed a -> Fixed a
 roundTo digits n = fromIntegral r / fromIntegral factor
  where
-  r :: Integer = round (n * fromIntegral factor)
-  factor       = 10 ^ digits :: Integer
+  r      = round (n * fromIntegral factor) :: Integer
+  factor = 10 ^ digits :: Integer
+
+showRounded :: HasResolution a => Int -> Fixed a -> String
+showRounded d nbr | d == 0    = take (x - 1) s
+                  | otherwise = take x s
+ where
+  s         = showFixed False (roundTo d nbr)
+  x         = length s - precision + d
+  precision = intLog10 (resolution nbr)
+
+intLog10 :: Integer -> Int
+intLog10 n | n < 10 = 0
+intLog10 n          = 1 + intLog10 (n `div` 10)

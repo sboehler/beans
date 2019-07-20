5727e8f80d149264e258db4989e7a8fbb28e9c7f
Silvio Böhler <sboehler@noreply.users.github.com>
2019-07-20 14:43:55 +0200

    Simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -1,32 +1,27 @@
 let
   sources = import ./nix/sources.nix;
 
-  overlays = [
-    (_: pkgs: {
-      haskell = pkgs.haskell // {
-        packageOverrides = self: super: {
-          ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
-          ghcWithPackages = self.ghc.withPackages;
-          ormolu = self.callCabal2nix "ormolu" sources.ormolu {};
-        };
-      };
-    })
-  ];
+  pkgs = import sources.nixpkgs {};
 
-  pkgs = import sources.nixpkgs {
-    overlays = overlays;
+  haskellPackages = pkgs.haskellPackages.override {
+    overrides = self: super: {
+      ghc = super.ghc // { withPackages = super.ghc.withHoogle; };
+      # ghcWithPackages = self.ghc.withPackages;
+      ormolu = self.callCabal2nix "ormolu" sources.ormolu {};
+    };
   };
 
-  drv = pkgs.haskellPackages.callPackage (import ./default.nix) {};
+  drv = haskellPackages.callPackage (import ./default.nix) {};
 
   drvWithTools = pkgs.haskell.lib.addBuildDepends drv (
-    with pkgs.haskellPackages; [
+    with haskellPackages; [
       ghcid
       cabal-install
       hlint
       hindent
       stylish-haskell
       ormolu
+      beam-core
     ]
     ++ (with pkgs.nodePackages; [pulp])
     ++ (with pkgs; [purescript psc-package])

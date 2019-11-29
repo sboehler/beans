1c66c931f7677a9e885fef35786a257a0477c273
Silvio Böhler <sboehler@noreply.users.github.com>
2019-11-29 10:39:04 +0100

    Rename

--- default.nix
+++ default.nix
@@ -1,22 +1,20 @@
 { mkDerivation, aeson, base, beam-core, beam-migrate, beam-sqlite
-, bytestring, cereal, conduit, cryptonite, dhall, hpack, jose
-, microlens-platform, optparse-applicative, resource-pool, rio
-, rio-orphans, servant, servant-auth, servant-auth-server
-, servant-conduit, servant-server, sqlite-simple, stdenv, text
-, time, transformers, wai, warp
+, bytestring, cereal, containers, hpack, microlens-platform
+, optparse-applicative, resource-pool, rio, rio-orphans, servant
+, servant-server, sqlite-simple, stdenv, text, time, transformers
+, wai, warp
 }:
 mkDerivation {
-  pname = "waccounts";
+  pname = "beans-web";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     aeson base beam-core beam-migrate beam-sqlite bytestring cereal
-    conduit cryptonite dhall jose microlens-platform
-    optparse-applicative resource-pool rio rio-orphans servant
-    servant-auth servant-auth-server servant-conduit servant-server
-    sqlite-simple text time transformers wai warp
+    containers microlens-platform optparse-applicative resource-pool
+    rio rio-orphans servant servant-server sqlite-simple text time
+    transformers wai warp
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [ base ];

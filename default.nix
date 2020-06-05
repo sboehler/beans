afb735d5fec3fb127adbc30c6f7d219f5fc048b5
Silvio Böhler <sboehler@noreply.users.github.com>
2020-06-06 00:56:45 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,41 +1,38 @@
-{ mkDerivation, aeson, base, bifunctors, bytestring, containers
-, dhall, directory, exceptions, filepath, groups, hpack
-, http-conduit, megaparsec, mtl, optparse-applicative
-, parser-combinators, prettyprinter, recursion-schemes, regex-pcre
-, scientific, stdenv, tasty, tasty-golden, tasty-hunit
+{ mkDerivation, aeson, base, bifunctors, bytestring, Cabal
+, containers, dhall, directory, either, exceptions, filepath, free
+, groups, hashable, hpack, http-conduit, megaparsec, mtl
+, optparse-applicative, parser-combinators, prettyprinter
+, regex-pcre, scientific, stdenv, tasty, tasty-golden, tasty-hunit
 , tasty-quickcheck, tasty-smallcheck, text, time
 , unordered-containers
 }:
 mkDerivation {
   pname = "beans";
-  version = "0.0.4.0";
+  version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    aeson base bifunctors bytestring containers dhall directory
-    exceptions filepath groups http-conduit megaparsec mtl
-    optparse-applicative parser-combinators prettyprinter
-    recursion-schemes regex-pcre scientific tasty tasty-golden
-    tasty-hunit tasty-quickcheck tasty-smallcheck text time
-    unordered-containers
+    aeson base bifunctors bytestring Cabal containers dhall directory
+    either exceptions filepath free groups hashable http-conduit
+    megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-smallcheck text time unordered-containers
   ];
   libraryToolDepends = [ hpack ];
   executableHaskellDepends = [
-    aeson base bifunctors bytestring containers dhall directory
-    exceptions filepath groups http-conduit megaparsec mtl
-    optparse-applicative parser-combinators prettyprinter
-    recursion-schemes regex-pcre scientific tasty tasty-golden
-    tasty-hunit tasty-quickcheck tasty-smallcheck text time
-    unordered-containers
+    aeson base bifunctors bytestring Cabal containers dhall directory
+    either exceptions filepath free groups hashable http-conduit
+    megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-smallcheck text time unordered-containers
   ];
   testHaskellDepends = [
-    aeson base bifunctors bytestring containers dhall directory
-    exceptions filepath groups http-conduit megaparsec mtl
-    optparse-applicative parser-combinators prettyprinter
-    recursion-schemes regex-pcre scientific tasty tasty-golden
-    tasty-hunit tasty-quickcheck tasty-smallcheck text time
-    unordered-containers
+    aeson base bifunctors bytestring Cabal containers dhall directory
+    either exceptions filepath free groups hashable http-conduit
+    megaparsec mtl optparse-applicative parser-combinators
+    prettyprinter regex-pcre scientific tasty tasty-golden tasty-hunit
+    tasty-quickcheck tasty-smallcheck text time unordered-containers
   ];
   prePatch = "hpack";
   homepage = "https://github.com/sboehler/beans#readme";
